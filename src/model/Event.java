f14d8ab44c8cc55ed8827917ae98d875ea35d0d7
Emma Vonbuelow <vonbuelow.e@northeastern.edu>
2024-03-10 14:38:36 -0400

    made uid a string (can be changed), added fields to the schedule class, and moreeee test cases

--- src/model/Event.java
+++ src/model/Event.java
@@ -4,9 +4,17 @@ import model.eventFields.Location;
 import model.eventFields.Time;
 
 public class Event {
+  String name;
+  Time time;
+  Location loc;
+  String uid;
 
-  Event(String name, Time time, Location loc, User uid) {
-
+  // i think User should be a string since it is a id
+  Event(String name, Time time, Location loc, String uid) {
+    this.name = name;
+    this.time = time;
+    this.loc = loc;
+    this.uid = uid;
   }
 
 }
--- src/model/Schedule.java
+++ src/model/Schedule.java
@@ -1,4 +1,14 @@
 package model;
 
+import java.util.List;
+
 public class Schedule {
+  String name;
+  List<Event> events;
+
+  Schedule(String name, List<Event> events) {
+    this.name = name;
+    this.events = events; // can be empty or have elements inside it
+  }
+
 }
