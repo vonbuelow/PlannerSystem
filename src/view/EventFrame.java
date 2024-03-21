10eb5cb3cfa09404db9376dc653c7c2a68dc8e99
noelisaponte <noelisaponte0@gmail.com>
2024-03-21 17:37:55 -0400

    comments for event frame impl

--- src/view/EventFrame.java
+++ src/view/EventFrame.java
@@ -10,4 +10,31 @@ public interface EventFrame {
   - the bottom most non-button component uses a JList
   - buttons to create, modify, or remove event
    */
+
+  /**
+   * Pseudocode:
+   * pops up when?
+   * - create/implement event
+   * - click on existing event
+   *   - pre-filled in w/ info
+   * wishlist:
+   * - close frame w/o ending program
+   * - on close, "transfer" (to model) info when either adding or modifying
+   *   - (save/send info somewhere?)
+   *
+   * Visual:
+   * - elements are stacked above each other
+   * ---------------------------------------------------------------
+   * text                                   | menu
+   *   event name                           |   is online/in person
+   *   location                             |   start/end day
+   *   time: start and end                  ------------------------
+   * -------------------------------------- | JList
+   *                                        |   available users
+   *
+   * - schedule event = all users in system
+   * - create event = only invite some users
+   *
+   *
+   */
 }
